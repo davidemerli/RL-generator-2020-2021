ee28e741a0edacaaf727da4cb6e7c0bc0024df86
Davide Merli <davide.merli@outlook.com>
2021-01-12 20:10:32 +0100

    feat: add pretty print as VHDL code snippets

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from math import floor, log2
 from random import randint
-import numpy as np
 
 
 def solve_batch(batch):
     """
     Given a list with a structure like
     [cols, rows, PIXEL_1, ..., PIXEL_(ROWS*COLS)]
-
     returns a list of pixels equalized with the given algorithm
     """
 
@@ -26,7 +24,6 @@ def solve_batch(batch):
 def generate_batch(cols, rows):
     """
     Given the number of columns and rows, returns a random pixel array with 2 + (rows * cols) elements
-
     The first two elements are the image dimensions as specified in the document
     """
 
@@ -36,10 +33,9 @@ def generate_batch(cols, rows):
 def print_image(ram):
     """
     Given a list representing ram values, visualizes the image before and after the equalization.
-
     Not needed for generation purposes.
     """
-    
+
     image = np.array(ram[2:-len(ram)//2 + 1])
     image = image.reshape(ram[1], ram[0])
 
@@ -61,6 +57,7 @@ def generate_ram(cols, rows):
 
 
 cols, rows = 4, 3
+
 
 def examples():
     """
@@ -89,26 +86,64 @@ def examples():
     print_image(ram)
 
 
-def main():
+def generate_raw_tests(to_generate=10):
     """
-    Generates 'TESTS' new tests, in the file tests.txt
+    Generates 'to_generate' new tests, in the file tests.txt
 
     Every test is a row with the test index, a closed bracket, and ram values for the test
     """
 
-    TESTS = 100
-
     with open('tests.txt', 'w') as test_file:
-        for i in range(TESTS):
-            cols, rows = randint(1, 129), randint(1, 129)
+        for i in range(to_generate):
+            cols, rows = randint(1, 128), randint(1, 128)
 
             ram = generate_ram(cols, rows)
             test_file.write(f'{i + 1}) {" ".join([str(x) for x in ram])}\n')
 
 
+def pretty_print_ram(ram):
+    """
+    Pretty prints a single ram configuration into 'tests.txt' and 'solution.txt'.
+    VHDL code snippets are written, to be copied and pasted into a test-bench file in Vivado.
+    """
+
+    test_file = open('test.txt', 'w')
+    sol_file = open('solution.txt', 'w')
+
+    bytes_length = int(len(ram) / 2 - 1)
+    test_file.write(f'{bytes_length}\n')
+
+    test_file.write("signal RAM: ram_type := (\n")
+
+    for element_position in range(bytes_length + 2):
+        test_file.write(f'\t\t\t{element_position} => std_logic_vector(to_unsigned({ram[element_position]}, 8)),\n')
+
+    test_file.write("\t\t\tothers => (others => '0'));\n\n")
+
+    for element_position in range(bytes_length + 2, 2 * bytes_length + 2):
+        sol_file.write(f'\tassert RAM({element_position}) = std_logic_vector(to_unsigned({ram[element_position]}, 8))')
+        sol_file.write(f' report \"TEST FALLITO (WORKING ZONE). ')
+        sol_file.write(f'Expected  {ram[element_position]}  ')
+        sol_file.write(f'found \" & integer\'image(to_integer(unsigned(RAM({element_position}))))  ')
+        sol_file.write(f'severity failure;\n')
+
+    print(f'Generated "test.txt" and "solution.txt" for a test with {bytes_length} bytes.')
+
+
+def main():
+    # UNCOMMENT TO GENERATE RAW TESTS
+    # generate_raw_tests()
+
+    # Generate a single random test
+    cols, rows = randint(1, 128), randint(1, 128)
+    ram = generate_ram(cols, rows)
+
+    # Pretty print into VHDL code snippets
+    pretty_print_ram(ram)
+
+
 if __name__ == '__main__':
     # UNCOMMENT THIS TO SHOW EXAMPLE
-    # examples() 
-    
-    main()
+    # examples()
 
+    main()
